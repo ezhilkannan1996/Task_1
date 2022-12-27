6d9a80ee2d164cf5c3eba2da3b900702317637e7
Ezhil Kannan <ezhilkannan1996@gmail.com>
2022-12-27 16:50:14 +0530

    Add project files.

---
+++ string split/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using string_split.Models;
+using System.Diagnostics;
+
+namespace string_split.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<SelectListItem> primeData = new List<SelectListItem>()
+        {
+            new SelectListItem{Text="One | Two | Three",Value="One | Two | Three"},
+            new SelectListItem{Text="1 | 2 | 3",Value="1 | 2 | 3"},
+            new SelectListItem{Text="Male | Jr.Developer | Chennai",Value="Male | Jr.Developer | Chennai"}
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.PrimData = primeData;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Entities demoData)
+        {
+            ViewBag.PrimData = primeData;
+            string One = demoData.dataOne;
+            string Two = demoData.dataTwo;
+            string Three = demoData.dataThree;
+            return View("DisplayView",demoData);
+        }
+    }
+}
---
+++ string split/Models/Entities.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace string_split.Models
+{
+    public class Entities
+    {
+        [Key]
+        public int? id { get; set; }
+        public string primaryData { get; set; }
+        public string dataOne { get; set; }
+        public string dataTwo { get; set; }
+        public string dataThree { get; set; }
+    }
+    public class PrimaryEntity
+    {
+        [Key]
+        public int? id { get; set; }
+        public string primaryData { get; set; }
+    }
+}
